d1c7430fcf320047bdc0d587619d6c6b32fe9dc1
em4nuelvt <emanuelvieiratavaresvt@gmail.com>
2024-09-11 22:12:58 -0300

    controle arduino

--- control.ino
+++ control.ino
@@ -4,6 +4,7 @@ const int relePin = 2;  // Pino de controle do relé
 int valorldr = 0; // Declara a variável valorldr como inteiro
 int valoresLDR[3]; // Declara um vetor de 3 posições para armazenar os valores do LDR
 int indice; // Variável para controlar o índice do vetor
+int media = 0;// Variável para calcular a média do valor obtido pelo sensor de luminosidade
 
 unsigned long releStartTime = 0;  // Marca o tempo de ativação do relé
 unsigned long lastMotorActivationTime = 0;  // Marca a última vez que o motor foi ativado
@@ -23,8 +24,8 @@ void setup() {
 void loop() {
   unsigned long currentTime = millis();  // Pega o tempo atual
 
-  // Lê o valor do sensor LDR e armazena no vetor a cada 5 segundos
-  if (currentTime - releStartTime >= 5000) {
+  // Lê o valor do sensor LDR e armazena no vetor a cada 1 segundo
+  if (currentTime - releStartTime >= 1000) {
     valorldr = analogRead(ldr);
     valoresLDR[indice] = valorldr;
 
@@ -33,7 +34,7 @@ void loop() {
 
     if (indice == 2) {
       // Calcula a média dos valores do LDR
-      int media = 0;
+      media=0
       for (int i = 0; i <= 2; i++) {
         media += valoresLDR[i];
       }
@@ -44,16 +45,17 @@ void loop() {
       indice = 0; // Reinicia o índice para novas leituras
 
       // Verifica se a média da luminosidade é maior que 50 e se já passaram 10 horas
-      if (media > 50 && (currentTime - lastMotorActivationTime >= motorInterval)) {
+      
+    } else {
+      indice++;  // Incrementa o índice para a próxima leitura
+    }
+    if (media > 50 && (currentTime - lastMotorActivationTime >= motorInterval)) {
         digitalWrite(relePin, HIGH);  // Liga o motor
         releOn = true;
         releStartTime = currentTime;  // Atualiza o tempo de início do motor
         lastMotorActivationTime = currentTime;  // Marca o tempo da última ativação
         Serial.println("Motor acionado.");
       }
-    } else {
-      indice++;  // Incrementa o índice para a próxima leitura
-    }
   }
 
   // Verifica se o motor já ficou ligado por 30 segundos
